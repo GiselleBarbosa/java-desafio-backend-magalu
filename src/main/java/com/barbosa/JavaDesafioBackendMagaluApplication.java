e3fe244184e1f3af2d6c1c082eab612a43252a4e
GiselleBarbosa <g88barbosa@gmail.com>
2024-11-06 08:43:59 -0300

    first commit

---
+++ src/main/java/com/barbosa/JavaDesafioBackendMagaluApplication.java
@@ -0,0 +1,13 @@
+package com.barbosa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDesafioBackendMagaluApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDesafioBackendMagaluApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/barbosa/JavaDesafioBackendMagaluApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbosa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDesafioBackendMagaluApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
